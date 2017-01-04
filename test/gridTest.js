53a93ddada60422012894c3257445093f53e0fa7
piolAtif <spreeti@thoughtworks.com>
2017-01-04 13:06:13 +0530

    Addeded test for 4*4 grid

--- test/gridTest.js
+++ test/gridTest.js
@@ -4,19 +4,16 @@ var Grid = require('../src/grid.js');
 describe('grid',function(){
 	
 	describe('next generation',function(){
-		var tableOf = function(grid){
-			return grid.nextGeneration().getTable();
-		};
 		
 		it('should return next generation for a 1*1 grid',function(){
 			var grid = new Grid(1,1);
 			grid.setCellAsAlive(0,0);
 
-			var generation = tableOf(grid);
+			var tableOfGrid = grid.nextGeneration().getTable();
 			var expected = [['D']];
 
-			assert.deepEqual(['D'], generation[0]);
-			assert.deepEqual(expected, generation);
+			assert.deepEqual(['D'], tableOfGrid[0]);
+			assert.deepEqual(expected, tableOfGrid);
 		});
 
 		it('should return next generation for a 2*2 grid',function(){
@@ -24,13 +21,13 @@ describe('grid',function(){
 			grid.setCellAsAlive(0,1);
 			grid.setCellAsAlive(1,1);
 
-			var generation = tableOf(grid);
+			var tableOfGrid = grid.nextGeneration().getTable();
 
 			var expected = [['D','D'],['D','D']];
 
-			assert.deepEqual(expected, generation);
-			assert.equal('D', generation[0][1]);
-			assert.equal('D', generation[1][1]);
+			assert.deepEqual(expected, tableOfGrid);
+			assert.equal('D', tableOfGrid[0][1]);
+			assert.equal('D', tableOfGrid[1][1]);
 		});
 
 		describe('generate a 3*3 grid',function(){
@@ -40,7 +37,7 @@ describe('grid',function(){
 			grid.setCellAsAlive(1,1);
 			grid.setCellAsAlive(2,1);
 
-			var generation = tableOf(grid);
+			var tableOfGrid = grid.nextGeneration().getTable();
 
 			//Tests===================================
 
@@ -48,43 +45,64 @@ describe('grid',function(){
 
 				var expected = [['D','D','D'],['A','A','A'],['D','D','D']];
 
-				assert.deepEqual(expected, generation);
-				assert.deepEqual(['A','A','A'],generation[1]);
+				assert.deepEqual(expected, tableOfGrid);
+				assert.deepEqual(['A','A','A'],tableOfGrid[1]);
 			});
 
 			it('third generation of grid should be same as initial',function(){
-				var nextGen = tableOf(grid);
+				var tableOfGrid = grid.nextGeneration().getTable();
 				var expected = [['D','A','D'],['D','A','D'],['D','A','D']];
 
-				assert.deepEqual(expected, nextGen);
-				assert.deepEqual(['D','A','D'], nextGen[0]);
+				assert.deepEqual(expected, tableOfGrid);
+				assert.deepEqual(['D','A','D'], tableOfGrid[0]);
 			});
 		});
 
 		describe('generate a 4*4 grid',function(){
-			var grid = new Grid(4,4);
-			grid.setCellAsAlive(1,1);
-			grid.setCellAsAlive(1,2);
-			grid.setCellAsAlive(2,0);
-			grid.setCellAsAlive(2,1);
-			grid.setCellAsAlive(2,2);
-			grid.setCellAsAlive(3,2);
+			var grid;
+			beforeEach(function(){
+				grid = new Grid(4,4);
+
+				grid.setCellAsAlive(1,1);
+				grid.setCellAsAlive(1,2);
+				grid.setCellAsAlive(2,0);
+				grid.setCellAsAlive(2,1);
+				grid.setCellAsAlive(2,2);
+				grid.setCellAsAlive(3,2);
+			});
+			
 
 			it('should return first generation of given grid',function(){
-				var generation = tableOf(grid);
+				var tableOfGrid = grid.nextGeneration().getTable();
 				var expected = [['D','D','D','D'],
 								['A','D','A','D'],
 								['A','D','D','A'],
 								['D','D','A','D']];
 
-				assert.deepEqual(expected, generation);
-				assert.deepEqual(['D','D','D','D'],generation[0]);
+				assert.deepEqual(expected, tableOfGrid);
+				assert.deepEqual(['D','D','D','D'],tableOfGrid[0]);
+			});
+
+			it('should return second generation for given grid',function(){
+				var generation = grid.nextGeneration();
+				var newGen = generation.nextGeneration();
+				var tableOfNewGrid = newGen.getTable();
+
+
+				var expected = [['D','D','D','D'],
+								['D','A','D','D'],
+								['D','D','A','A'],
+								['D','D','D','D']];
+
+				assert.deepEqual(expected, tableOfNewGrid);
+				assert.deepEqual(['D','A','D','D'], tableOfNewGrid[1]);
+
 			});
 
 			it('All cells should be dead in eight th generation',function(){
-				var generation;
+				var tableOfGrid;
 				for (var i = 0; i < 8; i++) {
-					generation = tableOf(grid);
+					tableOfGrid = grid.nextGeneration().getTable();
 				}
 
 				var expected = [['D','D','D','D'],
@@ -92,8 +110,8 @@ describe('grid',function(){
 								['D','D','D','D'],
 								['D','D','D','D']];
 
-				assert.deepEqual(expected, generation);
-				assert.deepEqual(['D','D','D','D'], generation[0]);
+				assert.deepEqual(expected, tableOfGrid);
+				assert.deepEqual(['D','D','D','D'], tableOfGrid[0]);
 
 			});
 		});
