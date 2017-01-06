6f9ce0d487cf2891393f8d4210ee9729e7f312da
piolAtif <spreeti@thoughtworks.com>
2017-01-06 10:01:08 +0530

    Added method for start and next method in game.js

--- game.js
+++ game.js
@@ -1,32 +1,53 @@
-var viceVersa = {'D':'A','A':'D'};
+var grid;
 
-var drawADiv = function(divToAdd, rangeFrom, rangeTo){
+var drawADiv = function(divToAdd, rangeFrom, rangeTo,gridToChange){
+	
 	divToAdd.append("div")
 	.style("width",50+'px')
 	.style("height",50+'px')
-	.text("D")
+	.text(gridToChange.table[rangeFrom][rangeTo])
 	.attr('id', 'd'+rangeFrom+''+rangeTo)
 	.on('click',function(){
 		var currDiv = d3.select('#d'+rangeFrom+''+rangeTo);
-		var content = viceVersa[currDiv.text()];
-		currDiv.text(content);
+		gridToChange.setCellAsAlive(rangeFrom, rangeTo);
+		currDiv.text('A');
 	});
+};
+
+var drawGrid = function(gridToDraw){
+	var table = gridToDraw.table;
+
+	var mainDiv = d3.select('#grid');
+	d3.selectAll('#childDiv').remove();
+
+	for (var i = 0; i < gridToDraw.rows; i++) {
+		var parentDiv = mainDiv.append('div').attr('id','childDiv');
+		for (var j = 0; j < gridToDraw.columns; j++) {
+			drawADiv(parentDiv,j,i, gridToDraw);
+		};
+	};
 }
 
 
 var createGrid = function(){
 	var row = document.getElementsByName('row')[0].value;
 	var column = document.getElementsByName('column')[0].value;
-	var grid = new Grid(row, column);
+	grid = new Grid(row, column);
 
-	var mainDiv = d3.select('#grid')
+	d3.select('#grid')
 		.style("width",function(){return (50*column)+column})
 		.style("height",function(){return(50*row)}+row);
 
-	for (var i = 0; i < grid.rows; i++) {
-		var parentDiv = mainDiv.append('div').attr('id','childDiv');
-		for (var j = 0; j < grid.columns; j++) {
-			drawADiv(parentDiv,j,i);
-		}
-	}
+	drawGrid(grid);
+
+};
+
+
+var next = function(){
+	grid = grid.nextGeneration();
+	drawGrid(grid);
+}
+
+var start = function(){
+	window.setInterval(next, 1000);
 }
